38f5749039dbffa8424cc8740ed0a08c573d61ac
may22802 <107598679+may22802@users.noreply.github.com>
2024-11-11 17:16:46 +0630

    functionality added

---
+++ index.js
@@ -0,0 +1,23 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let passwordOneEl = document.getElementById("password-one-el")
+let passwordTwoEl = document.getElementById("password-two-el")
+let generatePasswordBtnEl = document.getElementById("generate-passwords-btn-el")
+
+generatePasswordBtnEl.addEventListener("click", function(){
+    passwordOneEl.textContent = generatePasswords()
+    passwordTwoEl.textContent = generatePasswords()
+})
+
+function generatePasswords(){
+    let password = "";
+    for(let i = 0; i < 15; i++){
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex]
+    }
+
+    return password;
+}
+
